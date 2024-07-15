fdf1c35c5e07e122b5558374df7c6f2aa1d30fd6
peterhuff <peterhuff@wustl.edu>
2024-07-15 14:24:42 -0400

    Firebase setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+
+// import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyDjlegrZ8Z87TZr_xqApYq040In9Mfa64E",
+//   authDomain: "huff-website.firebaseapp.com",
+//   projectId: "huff-website",
+//   storageBucket: "huff-website.appspot.com",
+//   messagingSenderId: "195585843969",
+//   appId: "1:195585843969:web:ede1c0e5dc262bd399b100"
+// })
+
+// const firestore = firebase.firestore();
+
 function App() {
   return (
     <div className="App">
