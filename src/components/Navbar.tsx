88c83d12068afd9d53a5ec15563d05ef99c46b99
peterhuff <peterhuff@wustl.edu>
2024-07-15 21:56:18 -0400

    Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import '../styles/Navbar.css';
+
 export default function Navbar() {
     return (
-        <div>
-            
+        <div className="Navbar">
+            <div className="Title">
+                <h1>Peter Huff</h1>
+            </div>
+            <div className="Links">
+                <p>A</p>
+                <p>B</p>
+                <p>C</p>
+            </div>
         </div>
     );
 }
